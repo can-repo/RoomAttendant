87a7b7c63dc7d83aa28988ead511c22d4a3ebfc1
can <ichsan2409@gmail.com>
2025-11-19 14:54:34 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,22 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  base: '/', // Untuk deployment
+  server: {
+    host: '0.0.0.0', // Allow external connections
+    port: 5173, // Default Vite port
+    proxy: {
+      '/api': {
+        target: 'https://62f8bbc66050.ngrok-free.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api/1.0/json'),
+      },
     },
   },
 })
